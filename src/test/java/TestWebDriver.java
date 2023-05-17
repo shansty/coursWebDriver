a970f30e8690285314959d32317c9583295b01b2
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-17 20:13:40 +0300

    Initial commit

---
+++ src/test/java/TestWebDriver.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestWebDriver {
+    ChromeDriver driver;
+
+    @BeforeAll
+    public void prepareToStartTest(){
+        ChromeDriver driver = new ChromeDriver();
+        this.driver = driver;
+    }
+
+    @Test
+    public void testWebDriver() throws InterruptedException {
+        driver.get("https://pastebin.com");
+        WebElement kodeInput = driver.findElement(By.xpath("//*[@id=\"postform-text\"]"));
+        kodeInput.sendKeys("Hello from WebDriver");
+        WebElement pastName = driver.findElement(By.xpath("//*[@id=\"postform-name\"]"));
+        pastName.sendKeys("helloweb");
+        WebElement pasteExpiration = driver.findElement(By.id("select2-postform-expiration-container"));
+        pasteExpiration.click();
+        WebElement clickPasteExpiration = driver.findElement(By.xpath("//ul[@id='select2-postform-expiration-results']/li[text()='10 Minutes']"));
+        clickPasteExpiration.click();
+        WebElement submitBottom = driver.findElement(By.xpath("//*[@id=\"w0\"]/div[5]/div[1]/div[10]/button"));
+        submitBottom.click();
+        WebElement finalMessage = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[1]/div[4]/div[2]/ol/li/div"));
+        String actualResult = finalMessage.getText();
+        Assertions.assertEquals("Hello from WebDriver", actualResult);
+    }
+
+        @AfterAll
+        public void closeTests() throws InterruptedException {
+            Thread.sleep(4000);
+            driver.quit();
+        }
+
+}
